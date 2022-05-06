d9edea3e4952f86dde6144a6b6e468d50931ed86
Lieven Hollevoet <lieven@quicksand.be>
2022-05-06 15:25:05 +0200

    Allow earlier switch to 3-phase

--- bin/ev-dynacharge.pl
+++ bin/ev-dynacharge.pl
@@ -90,7 +90,7 @@ while (1) {
 			INFO "** Current is set to 0, not charging";
 		} else {
 			
-			if ($energy_balance > 4 && $nr_of_phases == 1 && $current > 15) {
+			if ($energy_balance > 1 && $nr_of_phases == 1 && $current > 15) {
 				# Switch to three-phase charging as there is room enough
 				INFO "Switching to three-phase charging based on $energy_balance W feed-in energy balance";
 				$nr_of_phases = 3;				
